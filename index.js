e0b05a41e5b7b7ab6db6fdec7a5c78feaa406893
Anh Nguyen <anhnguyendon92@gmail.com>
2020-08-20 12:16:36 -0700

    updates

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const inquirer = requirer('inquirer');
+const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('utill');
 
@@ -47,6 +47,82 @@ function promptUser() {
                 'GNU GPLv3'
             ],
             name: 'license'
-        }
-    ])
+        },
+        {
+            type: 'inpute',
+            message: ' Whose Credits is this work?',
+            name: 'credit'
+        },
+        {
+            type: 'input',
+            message: 'What is your GitHub username',
+            name: 'username'
+        },
+        {
+            type: 'input',
+            message: 'What is your email Address',
+            name: 'email'
+        },
+    ]);
 }
+
+function generateMarkdown(response) {
+    return `
+# ${response.title}
+
+# Table of Contense
+
+    - [Description](#description)
+    - [Installation](#installation)
+    - [usage](#usage)
+    - [Contributing](#contributing)
+    - [Test](#test)
+    - [Credits](#credits)
+    - [License](#license)
+    - [Questions](#questions)
+
+    ## Description:
+    ![License](https://img.sheilds.io/bagde/License-${response.licnese}-blue.svg lLicense Bagde)
+
+        ${response.description}
+    ## Installation:
+        ${response.installation}
+    ## Usage:
+        ${response.usage}
+    ## Contributing:
+        ${response.contribution}
+    ## Test:
+        ${response.test}
+    ## Credits:
+        ${response.credit}  
+    ## License:
+        For more infomation about the license, click on the link below.
+        
+    - [License](https://opensource.org/licenses/${response.license})  
+    
+    ## Questions:
+        For questions about the Generator you can go to my GitHub page at the following Link:
+
+    - [GitHub Profile](https://github.com/${response.username})
+    
+    For additional questions please reach out to my email at: ${response.email}.
+    `;
+
+}
+
+//Function to intialize program
+async function init() {
+    try {
+        const response = await promptUser();
+
+        const readMe = generateMarkdown(response);
+
+        await writeFileAsync('README.md', readMe);
+        console.log('Success');
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+//function call to intialize program
+init();
