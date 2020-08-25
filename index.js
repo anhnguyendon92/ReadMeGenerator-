8e1dfa4602c2147496c39e95071617cc82a4be24
Anh Nguyen <anhnguyendon92@gmail.com>
2020-08-25 12:06:03 -0700

    m

--- index.js
+++ index.js
@@ -1,74 +1,79 @@
-const inquirer = require('inquirer');
 const fs = require('fs');
-const util = require('util');
+
+const inquirer = require('inquirer');
+
 const axios = require('axios');
 
-const writeFileAsync = util.promisify(fs.writeFile);
-
-// function that crats the array of questions for user
-function promptUser() {
-    return inquirer.prompt([
-        {
-            type: 'input',
-            message: 'What is the name of your Project?',
-            name: 'title'
-        },
-        {
-            type: 'input',
-            message: 'Please enter a description of your project.',
-            name: 'description'
-        },
-        {
-            type: 'input',
-            message: 'What are the installation instructions fos this project. Write NONE if no instructions.',
-            name: 'installation'
-        },
-        {
-            type: 'input',
-            message: 'How would you like your application to be used?',
-            name: 'usage'
-        },
-        {
-            type: 'input',
-            message: 'Who contributed on this project?',
-            name: 'contribution'
-        },
-        {
-            type: 'input',
-            message: 'What are the Test Instructions??',
-            name: 'test'
-        },
-        {
-            type: 'checkbox',
-            message: 'Please select a license.',
-            choices: [
-                'Apache',
-                'MIT',
-                'ISC',
-                'GNU GPLv3'
-            ],
-            name: 'license'
-        },
-        {
-            type: 'inpute',
-            message: ' Whose Credits is this work?',
-            name: 'credit'
-        },
-        {
-            type: 'input',
-            message: 'What is your GitHub username',
-            name: 'username'
-        },
-        {
-            type: 'input',
-            message: 'What is your email Address',
-            name: 'email'
-        },
-    ]);
+const generateMarkdown = require('./utils/generateMarkdown.js')
+// array of questions for user
+const questions = [
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your GitHub username?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?'
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project Title?'
+    },
+    {
+        type: 'input',
+        name: 'repo',
+        message: 'What is the name of your repo?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'How do you describe your Project?.'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps required to install your project?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Write instructions for using your project.'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What license would you like to use',
+        choices: [
+            'MIT',
+            'Apache 2.0',
+            'GNU v3',
+            'Mozilla Public License 2.0',
+            'Unlicense'
+        ]
+    },
+    {
+        type: 'input',
+        name: 'contributors',
+        message: 'What are the rules for contributing to this project?'
+    },
+    {
+        type: 'input',
+        name: 'authors',
+        message: 'Who are the authors of this project?'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Provide examples on how to run tests.'
+    }
+];
+// function to write README file
+const writeToFile = (fileName, data) => {
+    fs.writeFile(fileName + '.md', data, error => error ? console.error(error) : console.log(`${fileName + '.md'} generated!`))
 }
-
-
-
 
 // function to initialize program
 const init = () => {
